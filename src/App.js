e845cf807b1eeec08a4d8db9a9ab5e0a68428849
Adrian Moya <moyaa1022@gmail.com>
2022-04-06 20:19:49 -0400

    feat(site): added basic functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,152 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, onValue } from "firebase/database";
+import {
+  getStorage,
+  ref as storageRef,
+  getDownloadURL,
+} from "firebase/storage";
+import "./App.css";
+import {
+  Typography,
+  Button,
+  Switch,
+  FormControlLabel,
+  TextField,
+  CssBaseline,
+} from "@mui/material";
+import { TimePicker } from "@mui/lab";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYC7pUCU8uW9GyLa2-eII1lE4OZxycwQo",
+  authDomain: "catfeeder-9781b.firebaseapp.com",
+  databaseURL: "https://catfeeder-9781b.firebaseio.com",
+  projectId: "catfeeder-9781b",
+  storageBucket: "catfeeder-9781b.appspot.com",
+  messagingSenderId: "1066991608255",
+  appId: "1:1066991608255:web:dc45932a9840338bbfc10a",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const database = getDatabase(firebaseApp);
+const storage = getStorage();
+
+const imageRef = storageRef(storage, "test.jpg");
+
+const feedNowRef = ref(database, "feed_now");
+const streamOnRef = ref(database, "stream_on");
+const timerOnRef = ref(database, "timer_on");
+const timerValueRef = ref(database, "timer_value");
 
 function App() {
+  const [feedNow, setFeedNow] = useState(false);
+  const [streamOn, setStreamOn] = useState(false);
+  const [timerOn, setTimerOn] = useState(false);
+  const [timerValue, setTimerValue] = useState(new Date());
+  const [streamUrl, setStreamUrl] = useState();
+  const [streamCacheBreaker, setStreamCacheBreaker] = useState(Date.now());
+
+  useEffect(() => {
+    onValue(feedNowRef, (snapshot) => setFeedNow(snapshot.val()));
+    onValue(streamOnRef, (snapshot) => setStreamOn(snapshot.val()));
+    onValue(timerOnRef, (snapshot) => setTimerOn(snapshot.val()));
+    onValue(timerValueRef, (snapshot) => setTimerValue(snapshot.val()));
+
+    getDownloadURL(imageRef).then((url) => setStreamUrl(url));
+  }, []);
+
+  useEffect(() => {
+    let interval = 0;
+    if (streamOn) {
+      interval = setInterval(() => {
+        setStreamCacheBreaker(Date.now());
+      }, 1000);
+    }
+    return () => {
+      console.log("Clearing");
+      clearInterval(interval);
+    };
+  }, [streamOn]);
+
+  /*
+    The input is only a time picker, but in the database it must be a datetime (because it is "next feeding")
+    So if the time selected has already passed today, then we set the date ot tomorrow. Otherwise, we set it to today.
+  */
+  const setScheduledDate = (scheduledDatetime) => {
+    const currentDateTime = new Date();
+    scheduledDatetime.setDate(currentDateTime.getDate());
+    if (currentDateTime >= scheduledDatetime) {
+      scheduledDatetime.setDate(scheduledDatetime.getDate() + 1);
+    }
+    console.log(
+      `current date: ${currentDateTime}\nNew date: ${scheduledDatetime}`
+    );
+    scheduledDatetime.setSeconds(0);
+    scheduledDatetime.setMilliseconds(0);
+    set(timerValueRef, scheduledDatetime.toISOString());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <div className="App">
+          <header className="App-header">
+            <Typography variant="h4">Tommy's Cat Feeder</Typography>
+          </header>
+          <div className="App-body">
+            <img
+              className="proof-of-life"
+              src={`${streamUrl}&cachebreaker=${streamCacheBreaker}`}
+              alt="Proof of life"
+            ></img>
+            <FormControlLabel
+              labelPlacement="start"
+              label="Camera"
+              control={
+                <Switch
+                  checked={streamOn}
+                  onChange={(event) => set(streamOnRef, event.target.checked)}
+                />
+              }
+            />
+            <FormControlLabel
+              labelPlacement="start"
+              label="Scheduled"
+              control={
+                <Switch
+                  checked={timerOn}
+                  onChange={(event) => {
+                    setScheduledDate(new Date(timerValue));
+                    set(timerOnRef, event.target.checked);
+                  }}
+                />
+              }
+            />
+            <TimePicker
+              label="Feeding Time"
+              disabled={!timerOn}
+              value={timerValue}
+              onChange={(newValue) => setTimerValue(newValue)}
+              onAccept={(newValue) => {
+                setScheduledDate(newValue);
+              }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+            <Button variant="contained">Feed that fat cat!</Button>
+          </div>
+        </div>
+      </ThemeProvider>
+    </LocalizationProvider>
   );
 }
 
